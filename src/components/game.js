3600ad354cfe9f29fb01cac50b8fa2b4eb36f133
Vlad <bladetheblade12@gmail.com>
2020-04-11 16:07:40 +0300

    Add logic to change Questions

--- src/components/game.js
+++ src/components/game.js
@@ -8,21 +8,45 @@ export default class Game extends Component {
     this.state = {
       questions: null,
       currentQuestion: null,
+      loading: true,
     };
   }
   async componentDidMount() {
     try {
       const questions = await loadQuestions();
-      this.setState({ questions, currentQuestion: questions[0] });
+      this.setState(
+        {
+          questions,
+        },
+        () => {
+          this.changeQuestion();
+        }
+      );
     } catch (err) {
       console.error(err);
     }
   }
 
+  changeQuestion = () => {
+    const randomQuestionIndex = Math.floor(
+      Math.random() * this.state.questions.length
+    );
+    const currentQuestion = this.state.questions[randomQuestionIndex];
+    const remainingQuestions = [...this.state.questions];
+    remainingQuestions.splice(randomQuestionIndex, 1);
+
+    this.setState({
+      questions: remainingQuestions,
+      currentQuestion,
+      loading: false,
+    });
+  };
+
   render() {
     return (
       <>
-        {this.state.currentQuestion && (
+        {this.state.loading && <div id="loader" />}
+        {!this.state.loading && this.state.currentQuestion && (
           <Question question={this.state.currentQuestion} />
         )}
       </>
