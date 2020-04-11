32e8ab15cf21227f65c26802915ed86574fc4057
Vlad <bladetheblade12@gmail.com>
2020-04-11 15:43:13 +0300

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import "./App.css";
 import Home from "./components/home";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import Game from "./components/Game";
+import HighScores from "./components/HighScores";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <div className="container">
+        <Route exact path="/" component={Home} />
+        <Route path="/game" component={Game} />
+        <Route path="/highScores" component={HighScores} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
